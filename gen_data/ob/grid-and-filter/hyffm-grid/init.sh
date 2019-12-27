e8f5f1db9167d581ae2e5413140e85f6ff5b820e
johncreed <hsiajuiyang5174@gmail.com>
2019-12-27 13:57:39 +0800

    Update init.sh

--- gen_data/ob/grid-and-filter/hyffm-grid/init.sh
+++ gen_data/ob/grid-and-filter/hyffm-grid/init.sh
@@ -3,6 +3,9 @@ machine_name=`hostname`
 if [ "$machine_name" = "peanuts" ]; then
   echo "$machine_name"
   source /home/skypole/intel/mkl/bin/mklvars.sh intel64
+elif [ "$machine_name" = "patty" ]; then
+  echo "$machine_name"
+  source /home/johncreed/intel/mkl/bin/mklvars.sh intel64
 elif [ "$machine_name" = "optima" ]; then
   echo "$machine_name"
   source /home/ybw/intel/mkl/bin/mklvars.sh intel64
