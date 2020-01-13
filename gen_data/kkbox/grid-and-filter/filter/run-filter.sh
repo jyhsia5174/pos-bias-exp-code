e89e80ce913c4eea88a3c4b8ee331aa558ca213c
johncreed <hsiajuiyang5174@gmail.com>
2020-01-13 15:22:39 +0800

    Update

--- gen_data/kkbox/grid-and-filter/filter/run-filter.sh
+++ gen_data/kkbox/grid-and-filter/filter/run-filter.sh
@@ -1,7 +1,7 @@
 item='item.ffm'
 tr5='rd.trva.ffm'
 tr90='filter.ffm'
-model='rd.ffm.cvt.4.-6.0.0009765625.1.ffm-ffm.model'
+model='filter.model'
 
 $time 
 echo "./predict-and-filter -c 5 $item $tr5 $tr90 $model"
