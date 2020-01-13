77dfe52be45b9d951b40fc4a751e58343c5379c3
johncreed <hsiajuiyang5174@gmail.com>
2020-01-13 17:11:13 +0800

    update

--- gen_data/kkbox/grid-and-filter/hyffm-grid/save_model.sh
+++ gen_data/kkbox/grid-and-filter/hyffm-grid/save_model.sh
@@ -8,6 +8,6 @@ w=0.25
 
 t=16
 
-./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm.cvt & 
+./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm & 
 
-echo "./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm.cvt" > filter.model.param
+echo "./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm" > filter.model.param
