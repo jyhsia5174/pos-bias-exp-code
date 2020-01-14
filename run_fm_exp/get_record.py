9b7b9b9e5110491d67405ec455626d52eb261f81
Mithril-NTU <d08944012@ntu.edu.tw>
2020-01-14 11:30:39 +0800

    -

--- run_fm_exp/get_record.py
+++ run_fm_exp/get_record.py
@@ -6,12 +6,20 @@ mode = sys.argv[2]
 f_path = os.path.join(root, "%s.record"%mode)
 va_num = [2] 
 te_num = list(range(4, 25, 4)) + list(range(5, 26, 4))
+logloss = list()
+auc = list()
 with open(f_path, 'r') as f:
     for i, line in enumerate(f):
         if i+1 in va_num:
-            print(line.strip().split(' ')[0])
-            print(line.strip().split(' ')[1])
+            logloss.append(line.strip().split(' ')[0])
+            auc.append(line.strip().split(' ')[1])
         if i+1 in te_num:
-            print(line.strip().split(' ')[-1])
+            if (i+1) % 2 == 0:
+                logloss.append(line.strip().split(' ')[-1])
+            else:
+                auc.append(line.strip().split(' ')[-1])
 
-
+for i in logloss:
+    print(i)
+for i in auc:
+    print(i)
