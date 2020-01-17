ad708e20a9598e9267438a67e78b9c993af348d4
Mithril-NTU <d08944012@ntu.edu.tw>
2020-01-18 02:45:11 +0800

    -

--- run_fm_exp/exp.sh
+++ run_fm_exp/exp.sh
@@ -144,7 +144,7 @@ do
 		done
 
 		ln -sf ${root}/${data_path}/der${i}${k}.imp/select_va.ffm ${cdir}/va.ffm
-		run_exp_imp ${cdir} ${root} ${mode} #| xargs -0 -d '\n' -P 1 -I {} sh -c {} 
+		run_exp_imp ${cdir} ${root} ${mode} | xargs -0 -d '\n' -P 1 -I {} sh -c {} 
 
 		cd ${cdir}
 		echo 'va_logloss va_auc' > ${mode}.record; 
