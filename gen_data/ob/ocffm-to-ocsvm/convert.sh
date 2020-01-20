2442474fbac92f83de6951e01b6da2603ecad7c3
johncreed <hsiajuiyang5174@gmail.com>
2020-01-20 13:04:59 +0800

    change conver.sh

--- gen_data/ob/ocffm-to-ocsvm/convert.sh
+++ gen_data/ob/ocffm-to-ocsvm/convert.sh
@@ -8,4 +8,4 @@ python ocffm-to-ocsvm.py item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm
 
 echo "tar --dereference -zcvf ob.300.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm "
 
-tar --dereference -zcvf kkbox.10.song.feat.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm 
+tar --dereference -zcvf ob.300.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm 
