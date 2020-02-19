de1a6a3e370985b08ebe5848dfd342094f48e097
johncreed <hsiajuiyang5174@gmail.com>
2020-02-19 15:04:29 +0800

    update

--- gen_data/kkbox.100.filter.test/grid-and-filter/hyffm-grid/save_model.sh
+++ gen_data/kkbox.100.filter.test/grid-and-filter/hyffm-grid/save_model.sh
@@ -10,7 +10,7 @@ tr='rd.trva.ffm.bin'
 # va logs
 logs='logs'
 cd ${logs}
-l=`cat * | sed '/iter/d' | sort -gk 3 | tail -n1  | xargs -I {}  grep --files-with-matches '{}' * `
+l=`cat * | sed '/iter/d' | sort -gk 3 | tail -n1  | xargs -I {}  grep --files-with-matches '{}' * |tail -n1`
 t=` cat * | sed '/iter/d' | sort -gk 3 | tail -n1  | awk '{print $1}' `
 echo $l $t
 cd ..
@@ -18,5 +18,5 @@ cd ..
 cmd="./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 10 -o filter.model item.ffm ${tr} > save_model.log"
 
 echo "${cmd}"
-#echo "${cmd}" > filter.model.param
-#echo "${cmd}" | xargs -I {} sh -c {} 
+echo "${cmd}" > filter.model.param
+echo "${cmd}" | xargs -I {} sh -c {} 
