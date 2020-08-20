33c33905a21f7702f6a1390382972d1279d3705b
jyhsia <hsiajuiyang5174@gmail.com>
2020-08-20 22:09:23 +0800

    Update save model script

--- data/data_preprocessing/kkbox-100/grid-and-filter/hyffm-grid/save_model.sh
+++ data/data_preprocessing/kkbox-100/grid-and-filter/hyffm-grid/save_model.sh
@@ -8,6 +8,6 @@ w=0.03125
 
 t=4
 
-./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm & 
+./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm 
 
 echo "./train -k 8 -l $l -t $t -r $r -w $w -wn 1  -c 5 -o filter.model item.ffm rd.trva.ffm" > filter.model.param
