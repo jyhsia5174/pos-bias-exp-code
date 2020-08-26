6db384e9713683b5ebef5eb6c46526d5a37939a1
jyhsia <hsiajuiyang5174@gmail.com>
2020-08-26 16:46:36 +0800

    Finish Pipeline

--- data/data_preprocessing/ob-300/grid-and-filter/filter/check.sh
+++ data/data_preprocessing/ob-300/grid-and-filter/filter/check.sh
@@ -2,13 +2,17 @@
 
 check(){
     echo $1
+    total_count=0
     for i in {1..10}
     do
-        awk -F , -v pos=$i '{print $pos}' $1 | grep ':1:' | wc -l
+        position_count=`awk -F , -v pos=$i '{print $pos}' $1 | grep ':1:' | wc -l`
+        total_count=`echo ${total_count} + ${position_count} | bc -l `
+        echo ${position_count} ${total_count}
     done
 
 }
 
-check determined_filter.label
-check random_filter.label
-check propensious_filter.label 
+check determined_filter.label > de_check.txt &
+check random_filter.label > rd_check.txt &
+check greedy_random_filter.label > gr_check.txt &
+check random_greedy_filter.label > rg_check.txt &
--- data/data_preprocessing/ob-300/ocffm-to-ocsvm/convert.sh
+++ data/data_preprocessing/ob-300/ocffm-to-ocsvm/convert.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-ext=$1
+ffm_ext=$1
+svm_ext="${ffm_ext/ffm/svm}"
 
-svm_ext="${ext/ffm/svm}"
+ffm_files="item.ffm det.${ffm_ext} random.${ffm_ext} 
+greedy_random.${ffm_ext} random_greedy.${ffm_ext} truth.ffm"
+svm_files="item.svm det.${svm_ext} random.${svm_ext} 
+greedy_random.${svm_ext} random_greedy.${svm_ext} truth.svm"
 
-python ocffm-to-ocsvm.py item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm
+# First file must be item.ffm 
+python ocffm-to-ocsvm.py ${ffm_files}
 
-echo "tar --dereference -zcvf ob.300.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm "
-
-tar --dereference -zcvf ob.300.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm 
+#echo "tar --dereference -zcvf ob.300.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm "
+#
+#tar --dereference -zcvf ob.300.${ext}.tar.gz item.ffm det.${ext} prop.${ext} random.${ext} truth.ffm item.svm det.${svm_ext} prop.${svm_ext} random.${svm_ext} truth.svm 
