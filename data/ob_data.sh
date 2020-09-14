26f4dca1fd93e5f08c26278b2b51d3ff6586d43b
Mithril-NTU <d08944012@ntu.edu.tw>
2020-09-14 14:39:25 +0800

    ob data processing script

---
+++ data/ob_data.sh
@@ -0,0 +1,25 @@
+ cd outbrain
+ mkdir RD
+ mkdir DR
+ mkdir cbias
+ mkdir normal
+ mv random_greedy.* RD
+ mv greedy_random.* DR
+ mv *unif* cbias
+ cp truth.* item.* RD
+ cp truth.* item.* DR
+ cp truth.* item.* cbias
+ mv det.* random.* item.* truth.* normal
+ cd RD
+ ln -sf random_greedy.ffm.pos.0.5.bias det.ffm.pos.0.5.bias
+ ln -sf random_greedy.svm.pos.0.5.bias det.svm.pos.0.5.bias
+ cd ../
+ cd DR
+ ln -sf greedy_random.svm.pos.0.5.bias det.svm.pos.0.5.bias
+ ln -sf greedy_random.ffm.pos.0.5.bias det.ffm.pos.0.5.bias
+ cd ../
+ cd cbias
+ ln -sf random.ffm.pos.0.5.unif.bias det.ffm.pos.0.5.bias
+ ln -sf random.svm.pos.0.5.unif.bias det.svm.pos.0.5.bias
+ cd ../
+ cd ../
